cd987321eca0c0a346b1cde437591b5cf469b5e2
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2024-07-11 00:42:08 +0530

    full setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-;
+import express, { Express, Request, Response } from 'express';
+
+const port = Number(process.env.PORT) || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
